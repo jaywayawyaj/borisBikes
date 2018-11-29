9318e789f03ab4117a2ba02c3c56f9f5a5533da3
Jacques <jacques.laffy@gmail.com>
2018-11-29 16:09:46 +0000

    Cannot dock Bike if Station Full --> Ex 13

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,14 @@
 class DockingStation
-
   attr_reader :bike
 
   def release_bike
-   fail "No bikes available" unless @bike
-   @bike
+    fail "No bikes available" unless @bike
+    @bike
   end
 
   def dock(bike)
-    @bike = bike
+    fail 'Docking Station Full' unless @bike == nil
+    @bike = bike 
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,27 +1,36 @@
 require 'docking_station'
 describe DockingStation do
-  it 'It responds to release_bike' do
-    docking_station = DockingStation.new
-    expect{docking_station.to respond_to(:release_bike)}
+  describe "#release_bike" do
+
+    it 'should cause an error if the Docking Station is empty' do
+      docking_station = DockingStation.new
+      expect{docking_station.release_bike}.to raise_error
+    end
+
+    it 'It responds to release_bike' do
+      docking_station = DockingStation.new
+      expect{docking_station.to respond_to(:release_bike)}
+    end
+
+    it 'It returns a working bike' do
+      bike = Bike.new
+      expect{bike.working?.eql?(true)}
+    end
   end
 
-  it 'It returns a working bike' do
-    bike = Bike.new
-    expect{bike.working?.eql?(true)}
-  end
+  describe "#dock" do
 
-  it "It should respond to dock" do
-    docking_station = DockingStation.new
-    expect{docking_station.to respond_to(:dock)}
-  end
+    it 'should cause an error if the Docking Station is full' do
+      bike = Bike.new
+      bike_2 = Bike.new
+      docking_station = DockingStation.new
+      docking_station.dock(bike)
+      expect{docking_station.dock(bike_2)}.to raise_error 'Docking Station Full'
+    end
 
-  it 'docks something' do
+    it 'docks something' do
       bike = Bike.new
       expect(subject.dock(bike)).to eq bike
-  end
-
-  it 'should cause an error if the Docking Station is empty' do
-    docking_station = DockingStation.new
-    expect{docking_station.release_bike}.to raise_error
+    end
   end
 end
